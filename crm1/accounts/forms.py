2ff93f2a3d006fdd8b6396199c401de4bf3bf67e
rafsunsheikh <rafsun.sheikh@gmail.com>
2022-08-11 00:55:21 +0600

    added create order form

---
+++ crm1/accounts/forms.py
@@ -0,0 +1,14 @@
+from dataclasses import field
+from pyexpat import model
+from django.forms import ModelForm
+from .models import Customer, Order
+
+class OrderForm(ModelForm):
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+class CustomerForm(ModelForm):
+    class Meta:
+        model = Customer
+        fields = '__all__'
--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+from .forms import OrderForm, CustomerForm
 
 def home(request):
     customers  = Customer.objects.all()
@@ -25,3 +27,28 @@ def customer(request, pk):
 
     context = {'customer':customer, 'orders':orders, 'orders_count':orders_count}
     return render(request, 'accounts/customer.html', context)
+
+def createOrder(request):
+
+    form  = OrderForm()
+    if request.method == 'POST':
+        form  = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form':form}
+
+    return render(request, 'accounts/create_order.html', context)
+
+def createCustomer(request):
+    form = CustomerForm()
+    if request.method == 'POST':
+        form = CustomerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form':form}
+    return render(request, 'accounts/create_customer.html', context)
+
